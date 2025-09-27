efa41890a91ebc959f8d9e590980aeefabda9e62
bentahsin <tahsin@bentahsin.com>
2025-09-27 11:41:37 +0300

    feat: ServerPlaceholders sınıfında oyuncu parametresi eklendi

--- src/main/java/me/agnes/agnesesle/placeholders/ServerPlaceholders.java
+++ src/main/java/me/agnes/agnesesle/placeholders/ServerPlaceholders.java
@@ -3,6 +3,7 @@ package me.agnes.agnesesle.placeholders;
 import com.bentahsin.benthpapimanager.annotations.Placeholder;
 import com.bentahsin.benthpapimanager.annotations.PlaceholderIdentifier;
 import me.agnes.agnesesle.data.EslestirmeManager;
+import org.bukkit.OfflinePlayer;
 
 @Placeholder(identifier = "agnesesle_server", author = "Agnes & bentahsin", version = "1.2")
 public class ServerPlaceholders {
@@ -12,7 +13,7 @@ public class ServerPlaceholders {
      * Kullanım: %agnesesle_server_toplam_eslesme%
      */
     @PlaceholderIdentifier(identifier = "toplam_eslesme", onError = "0")
-    public String getToplamEslesme() {
+    public String getToplamEslesme(OfflinePlayer player) {
         return String.valueOf(EslestirmeManager.getTumEslesmeler().size());
     }
 }
